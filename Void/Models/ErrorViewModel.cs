df7cc16c2a14489c6e084514aac9c650684e7b0f
nomorevalid <32668123+nomorevalid@users.noreply.github.com>
2022-06-24 00:38:01 +0300

    Add project files.

---
+++ Void/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Void.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
